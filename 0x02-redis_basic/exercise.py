c5038eac76df9552b2d199c9f71ee36207fe7a4c
Mosesomo <moseswamboga4@gmail.com>
2024-03-29 19:04:54 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Writing strings to Redis'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional
 
 
 class Cache:
@@ -28,3 +28,47 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+    
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        '''
+            method that take a key string argument and an optional Callable
+            argument named fn. This callable will be used to convert the
+            data back to the desired format.
+        '''
+
+        data = self._redis.get(key)
+        if fn:
+            fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''parametrize Cache.get with the correct conversion function.'''
+
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        '''parametrize Cache.get with the correct conversion function.'''
+
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except Exception:
+            data = 0
+
+        return data
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+    
+    
