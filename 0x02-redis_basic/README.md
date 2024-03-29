f8b9ad5a912602c8483763db3d976c8a7b1d68c5
Mosesomo <moseswamboga4@gmail.com>
2024-03-29 18:22:09 +0300

    redis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,5 @@
+Redis Basics
+----------------------------------------------------
+Redis is an open-source, in-memory data structure store that can be used as a database, cache, and message broker. It is commonly used for tasks like caching, real-time analytics, messaging queues, and more. Redis supports various data structures such as strings, hashes, lists, sets, sorted sets with range queries, bitmaps, hyperloglogs, geospatial indexes with radius queries, and streams.
+
+In Python, you can use Redis by installing the redis package, which provides a Python client for Redis. This client allows you to interact with a Redis server from your Python code, enabling you to store and retrieve data, perform transactions, publish and subscribe to messages, and execute various commands supported by Redis.
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Writing strings to Redis'''
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    '''A simple cache class for
+        writing data to a Redis server.
+    '''
+
+    def __init__(self) -> None:
+        '''Instance of redis client'''
+
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+        
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''
+            store method that takes a data argument and returns
+            a string. The method should generate a random key
+            (e.g. using uuid), store the input data
+            in Redis using the random key and return the key.
+        '''
+
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        
+        return key
